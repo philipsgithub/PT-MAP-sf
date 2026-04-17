a75386de0dbd254d3e91bf4c1752a10a5cb93da5
Aleksandra Marta Wrzeszcz <yx104@tks-hertz.fzi.de>
2026-04-17 16:26:28 +0200

    added filter size as param, as it is needed for test

--- io_utils.py
+++ io_utils.py
@@ -60,7 +60,8 @@ def parse_args(script):
         parser.add_argument('--split'       , default='novel', help='base/val/novel') #default novel, but you can also test base/val class accuracy if you want 
         parser.add_argument('--save_iter', default=-1, type=int,help ='saved feature from the model trained in x epoch, use the best model if x is -1')
         parser.add_argument('--adaptation'  , action='store_true', help='further adaptation in test time or not')
-        parser.add_argument('--num_classes' , default=200, type=int, help='total number of classes')  
+        parser.add_argument('--num_classes' , default=200, type=int, help='total number of classes')
+        parser.add_argument('--filter_size', default=8, help='size of DCT filter, per default 8.')
     else:
        raise ValueError('Unknown script')
         
